8d595906974c88bc51185576e4993a2558d6644d
Wolfgang Kluth <me@kluth.co.uk>
2014-06-02 14:44:42 +0200

    update transaction view and fix delete pedelecs bug

--- src/main/webapp/scripts/transaction/router.js
+++ src/main/webapp/scripts/transaction/router.js
@@ -1,8 +1,8 @@
 'use strict';
 
 velocityApp
-    .config(['$stateProvider', '$httpProvider', '$translateProvider',
-        function ($stateProvider, $httpProvider, $translateProvider) {
+    .config(['$stateProvider', '$httpProvider', '$translateProvider', 'USER_ROLES',
+        function ($stateProvider, $httpProvider, $translateProvider, USER_ROLES) {
             $stateProvider
                 .state('transaction', {
                     url: '/transaction',
@@ -12,6 +12,35 @@ velocityApp
                         resolvedTransaction: ['Transaction', function (Transaction) {
                             return Transaction.query();
                         }]
+                    },
+                    access: {
+                        authorizedRoles: [USER_ROLES.all]
+                    }
+                })
+                .state('closedTransaction', {
+                    url: '/closed-transaction',
+                    templateUrl: 'views/transactions.html',
+                    controller: 'TransactionController',
+                    resolve:{
+                        resolvedTransaction: ['Transaction', function (Transaction) {
+                            return Transaction.query();
+                        }]
+                    },
+                    access: {
+                        authorizedRoles: [USER_ROLES.all]
+                    }
+                })
+                .state('openTransaction', {
+                    url: '/open-transaction',
+                    templateUrl: 'views/transactions.html',
+                    controller: 'TransactionController',
+                    resolve:{
+                        resolvedTransaction: ['Transaction', function (Transaction) {
+                            return Transaction.query();
+                        }]
+                    },
+                    access: {
+                        authorizedRoles: [USER_ROLES.all]
                     }
                 })
         }]);
