e2bccb8e0af7e2867b9f9a77d4d3a28fd782a110
Wolfgang Kluth <me@kluth.co.uk>
2014-06-20 13:44:28 +0200

    update exceptions and delete unnecessary resources

--- src/main/java/de/rwth/idsg/velocity/repository/StationSlotRepository.java
+++ src/main/java/de/rwth/idsg/velocity/repository/StationSlotRepository.java
@@ -1,10 +1,10 @@
 package de.rwth.idsg.velocity.repository;
 
 import de.rwth.idsg.velocity.domain.StationSlot;
-        import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Spring Data JPA repository for the StationSlot entity.
+ * Created by swam on 18/06/14.
  */
 public interface StationSlotRepository extends JpaRepository<StationSlot, Long> {
 
