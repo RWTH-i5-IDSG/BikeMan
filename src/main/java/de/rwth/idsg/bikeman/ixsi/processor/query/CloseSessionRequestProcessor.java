0b6d6c6e955742d9f56d6ef78044d29d81f5d15c
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2014-10-23 13:31:54 +0200

    working on ixsi

--- src/main/java/de/rwth/idsg/bikeman/ixsi/processor/query/CloseSessionRequestProcessor.java
+++ src/main/java/de/rwth/idsg/bikeman/ixsi/processor/query/CloseSessionRequestProcessor.java
@@ -1,10 +1,13 @@
 package de.rwth.idsg.bikeman.ixsi.processor.query;
 
 import com.google.common.base.Optional;
+import de.rwth.idsg.bikeman.ixsi.ErrorFactory;
 import de.rwth.idsg.bikeman.ixsi.schema.AuthType;
 import de.rwth.idsg.bikeman.ixsi.schema.CloseSessionRequestType;
 import de.rwth.idsg.bikeman.ixsi.schema.CloseSessionResponseType;
+import de.rwth.idsg.bikeman.ixsi.schema.ErrorType;
 import de.rwth.idsg.bikeman.ixsi.schema.Language;
+import de.rwth.idsg.bikeman.ixsi.schema.OpenSessionResponseType;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,11 +19,22 @@ public class CloseSessionRequestProcessor extends AbstractUserRequestProcessor<C
 
     @Override
     public UserResponseParams<CloseSessionResponseType> processAnonymously(Optional<Language> lan, CloseSessionRequestType request) {
-        return super.processAnonymously(lan, request);
+        return respondWithError();
     }
 
     @Override
     public UserResponseParams<CloseSessionResponseType> processForUser(Optional<Language> lan, AuthType auth, CloseSessionRequestType request) {
-        return super.processForUser(lan, auth, request);
+        return respondWithError();
+    }
+
+    private UserResponseParams<CloseSessionResponseType> respondWithError() {
+        ErrorType e = ErrorFactory.requestNotSupported();
+
+        CloseSessionResponseType o = new CloseSessionResponseType();
+        o.getError().add(e);
+
+        UserResponseParams<CloseSessionResponseType> u = new UserResponseParams<>();
+        u.setResponse(o);
+        return u;
     }
 }
--- src/main/java/de/rwth/idsg/bikeman/ixsi/processor/query/OpenSessionRequestProcessor.java
+++ src/main/java/de/rwth/idsg/bikeman/ixsi/processor/query/OpenSessionRequestProcessor.java
@@ -1,7 +1,9 @@
 package de.rwth.idsg.bikeman.ixsi.processor.query;
 
 import com.google.common.base.Optional;
+import de.rwth.idsg.bikeman.ixsi.ErrorFactory;
 import de.rwth.idsg.bikeman.ixsi.schema.AuthType;
+import de.rwth.idsg.bikeman.ixsi.schema.ErrorType;
 import de.rwth.idsg.bikeman.ixsi.schema.Language;
 import de.rwth.idsg.bikeman.ixsi.schema.OpenSessionRequestType;
 import de.rwth.idsg.bikeman.ixsi.schema.OpenSessionResponseType;
@@ -14,14 +16,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class OpenSessionRequestProcessor extends AbstractUserRequestProcessor<OpenSessionRequestType, OpenSessionResponseType> {
 
-
     @Override
     public UserResponseParams<OpenSessionResponseType> processAnonymously(Optional<Language> lan, OpenSessionRequestType request) {
-        return super.processAnonymously(lan, request);
+        return respondWithError();
     }
 
     @Override
     public UserResponseParams<OpenSessionResponseType> processForUser(Optional<Language> lan, AuthType auth, OpenSessionRequestType request) {
-        return super.processForUser(lan, auth, request);
+        return respondWithError();
     }
+
+    private UserResponseParams<OpenSessionResponseType> respondWithError() {
+        ErrorType e = ErrorFactory.requestNotSupported();
+
+        OpenSessionResponseType o = new OpenSessionResponseType();
+        o.getError().add(e);
+
+        UserResponseParams<OpenSessionResponseType> u = new UserResponseParams<>();
+        u.setResponse(o);
+        return u;
+    }
+
 }
