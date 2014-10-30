3df3e79ad538af3bc9bb4f2e274260eb5bcc8798
Max Wiederhold <maximilian.wiederhold@rwth-aachen.de>
2014-10-30 16:34:41 +0100

    AvailabilityRequest implemented in repository

--- src/main/java/de/rwth/idsg/bikeman/ixsi/dto/query/AvailabilityResponseDTO.java
+++ src/main/java/de/rwth/idsg/bikeman/ixsi/dto/query/AvailabilityResponseDTO.java
@@ -1,22 +1,42 @@
 package de.rwth.idsg.bikeman.ixsi.dto.query;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.Value;
+import org.hibernate.transform.ResultTransformer;
 
+import javax.persistence.ColumnResult;
+import javax.persistence.ConstructorResult;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.SqlResultSetMapping;
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by max on 06/10/14.
  */
-@AllArgsConstructor
-@Getter
+@Data
+//@SqlResultSetMapping(name = "availabilityResult", classes = {
+//        @ConstructorResult(targetClass = AvailabilityResponseDTO.class,
+//                columns = {
+//                        @ColumnResult(name = "pedelecId"),
+//                        @ColumnResult(name = "stationId"),
+//                        @ColumnResult(name = "locationLatitude"),
+//                        @ColumnResult(name = "locationLongitude"),
+//                        @ColumnResult(name = "stateOfCharge")
+//                })
+//})
 public class AvailabilityResponseDTO {
 
-    private long pedelecId;
-    private long stationId;
-    private BigDecimal locationLatitude;
-    private BigDecimal locationLongitude;
-    private Float stateOfCharge;
+    private final BigInteger pedelecId;
+    private final BigInteger stationId;
+    private final BigDecimal locationLatitude;
+    private final BigDecimal locationLongitude;
+    private final Float stateOfCharge;
 //    private Float drivingRange;
+
 
 }
--- src/main/java/de/rwth/idsg/bikeman/ixsi/repository/QueryIXSIRepositoryImpl.java
+++ src/main/java/de/rwth/idsg/bikeman/ixsi/repository/QueryIXSIRepositoryImpl.java
@@ -13,12 +13,22 @@ import de.rwth.idsg.bikeman.ixsi.schema.GeoCircleType;
 import de.rwth.idsg.bikeman.ixsi.schema.GeoRectangleType;
 import de.rwth.idsg.bikeman.ixsi.schema.ProviderPlaceIDType;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Created by max on 06/10/14.
@@ -84,8 +94,37 @@ public class QueryIXSIRepositoryImpl implements QueryIXSIRepository {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<AvailabilityResponseDTO> availability(GeoCircleType circle) {
-        return null;
+        Integer r = circle.getRadius();
+        BigDecimal lat = circle.getCenter().getLatitude();
+        BigDecimal lon = circle.getCenter().getLongitude();
+        final String availQuery = "SELECT " +
+                                  "p.pedelec_Id as pedelecId, s.station_Id as stationId, " +
+                                  "s.location_Latitude as locationLatitude, s.location_Longitude as locationLongitude, p.state_Of_Charge as stateOfCharge " +
+                                  "FROM t_Pedelec p JOIN t_Station_Slot slot ON p.pedelec_Id = slot.pedelec_Id " +
+                                  "JOIN t_Station s ON s.station_Id = slot.station_Id " +
+                                  "WHERE st_intersects(" +
+                                  "st_geometryfromtext('POINT( ' || s.location_Latitude || ' '" +
+                                  " || s.location_Longitude || ')')," +
+                                  "st_buffer(CAST(st_makepoint(" + lat + ", " + lon + ") as geography), " + r + "))";
+
+
+        Query q = em.createNativeQuery(availQuery);
+        List<AvailabilityResponseDTO> myList = new ArrayList<>();
+
+        List<Object[]> fooList = q.getResultList();
+        for (Object[] row : fooList) {
+            AvailabilityResponseDTO dto = new AvailabilityResponseDTO(
+                    (BigInteger) row[0],
+                    (BigInteger) row[1],
+                    (BigDecimal) row[2],
+                    (BigDecimal) row[3],
+                    (Float) row[4]);
+
+            myList.add(dto);
+        }
+        return myList;
     }
 
     @Override
