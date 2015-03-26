d0c3a2a8992905c8fb5a93f8c7341e8f19a7e6df
Wolfgang Kluth <me@kluth.co.uk>
2015-03-26 12:59:57 +0100

    moved media type produce declaration into requestMapping

--- src/main/java/de/rwth/idsg/bikeman/app/resource/StationResource.java
+++ src/main/java/de/rwth/idsg/bikeman/app/resource/StationResource.java
@@ -7,20 +7,18 @@ import de.rwth.idsg.bikeman.app.exception.AppException;
 import de.rwth.idsg.bikeman.app.service.StationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 
 @RestController("StationResourceApp")
 // TODO: change mapping path
-@RequestMapping("/apptest")
-@Produces(MediaType.APPLICATION_JSON)
+@RequestMapping(value = "/apptest", produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 public class StationResource {
 
