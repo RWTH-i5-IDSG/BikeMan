55d05636ad1bb84419625883a3816e80e1cbea18
Michael712 <michael.gitrepo@zwanzig-zehn.de>
2015-04-25 14:55:53 +0200

    Add CORS-filter to allow access from all webpages

---
+++ src/main/java/de/rwth/idsg/bikeman/config/CORSFilter.java
@@ -0,0 +1,29 @@
+package de.rwth.idsg.bikeman.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        //TODO: this allows access from _ALL_ origins; allow only specific locations in non-development environments!
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {}
+}
