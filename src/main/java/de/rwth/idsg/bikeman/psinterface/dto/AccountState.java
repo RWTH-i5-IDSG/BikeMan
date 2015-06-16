e7ffce41182d25789da5aae44ffd0c75a86a8e9c
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2015-06-16 21:26:55 +0200

    allowed the enum values in requests to be case insensitive

--- src/main/java/de/rwth/idsg/bikeman/psinterface/dto/AccountState.java
+++ src/main/java/de/rwth/idsg/bikeman/psinterface/dto/AccountState.java
@@ -1,9 +1,27 @@
 package de.rwth.idsg.bikeman.psinterface.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * Created by swam on 27/04/15.
  */
 public enum AccountState {
     HAS_PEDELEC,
-    HAS_NO_PEDELEC
+    HAS_NO_PEDELEC;
+
+    @JsonValue // serialize
+    public String value() {
+        return name();
+    }
+
+    @JsonCreator // deserialize
+    public static AccountState fromValue(String v) {
+        for (AccountState c : AccountState.values()) {
+            if (c.name().equalsIgnoreCase(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
 }
--- src/main/java/de/rwth/idsg/bikeman/psinterface/dto/LogsUpdateState.java
+++ src/main/java/de/rwth/idsg/bikeman/psinterface/dto/LogsUpdateState.java
@@ -1,9 +1,27 @@
 package de.rwth.idsg.bikeman.psinterface.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * Created by swam on 01/08/14.
  */
 public enum LogsUpdateState {
     UPLOADED,
-    UPLOAD_FAILED
+    UPLOAD_FAILED;
+
+    @JsonValue // serialize
+    public String value() {
+        return name();
+    }
+
+    @JsonCreator // deserialize
+    public static LogsUpdateState fromValue(String v) {
+        for (LogsUpdateState c : LogsUpdateState.values()) {
+            if (c.name().equalsIgnoreCase(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
 }
