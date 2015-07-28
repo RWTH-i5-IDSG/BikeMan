2fda9a6f4f6b50172552f37a64483beb68ef87eb
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2015-07-28 17:33:37 +0200

    removed unnecessary explicit settings since these are already done under the covers

--- src/main/java/de/rwth/idsg/bikeman/config/PsInterfaceConfiguration.java
+++ src/main/java/de/rwth/idsg/bikeman/config/PsInterfaceConfiguration.java
@@ -1,10 +1,7 @@
 package de.rwth.idsg.bikeman.config;
 
-import de.rwth.idsg.bikeman.psinterface.rest.client.HeaderInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -16,10 +13,6 @@ public class PsInterfaceConfiguration {
 
     @Bean
     public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-        restTemplate.getInterceptors().add(new HeaderInterceptor());
-        return restTemplate;
+        return new RestTemplate();
     }
 }
