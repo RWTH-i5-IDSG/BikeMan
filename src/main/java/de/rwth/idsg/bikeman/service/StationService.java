3c1993d7fadfd91075d8d7ecd3fcb22295b9157c
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2015-10-05 14:54:01 +0200

    fix: do not set slot position when changing the state of station

--- src/main/java/de/rwth/idsg/bikeman/service/StationService.java
+++ src/main/java/de/rwth/idsg/bikeman/service/StationService.java
@@ -67,7 +67,6 @@ public class StationService {
     public void updateStation(CreateEditStationDTO dto) throws DatabaseException {
         ChangeStationOperationStateDTO changeDTO = new ChangeStationOperationStateDTO();
         changeDTO.setState(dto.getState());
-        changeDTO.setSlotPosition(-1);
 
         // first, communicate with the station to update status
         // then, update in DB
