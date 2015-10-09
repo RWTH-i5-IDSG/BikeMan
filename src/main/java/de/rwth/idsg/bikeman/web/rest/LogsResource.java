1c379614c21717b2cc976a3ced69cd75fe6551f4
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2015-10-09 11:39:51 +0200

    fix: a less fragile approach to get log file path

--- src/main/java/de/rwth/idsg/bikeman/web/rest/LogsResource.java
+++ src/main/java/de/rwth/idsg/bikeman/web/rest/LogsResource.java
@@ -1,13 +1,16 @@
 package de.rwth.idsg.bikeman.web.rest;
 
 import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
+import ch.qos.logback.core.FileAppender;
 import com.codahale.metrics.annotation.Timed;
-import de.rwth.idsg.bikeman.config.Constants;
+import com.google.common.base.Optional;
 import de.rwth.idsg.bikeman.web.rest.dto.LoggerDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -25,6 +27,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,14 +39,13 @@ import java.util.stream.Collectors;
 @RequestMapping("/api")
 public class LogsResource {
 
-    @Inject private Environment env;
-
     private Path logPath;
 
     @PostConstruct
     private void init() {
-        if (env.acceptsProfiles(Constants.SPRING_PROFILE_PRODUCTION)) {
-            logPath = Paths.get(System.getProperty("catalina.base"), "logs", "bikeman.log");
+        Optional<String> optionalPath = getActiveLogFile();
+        if (optionalPath.isPresent()) {
+            logPath = Paths.get(optionalPath.get());
         }
     }
 
@@ -74,8 +76,7 @@ public class LogsResource {
 
         try (PrintWriter writer = response.getWriter()) {
             if (logPath == null) {
-                writer.write("Not available, because BikeMan is not running in '"
-                        + Constants.SPRING_PROFILE_PRODUCTION + "' profile");
+                writer.write("Not available. Log file does not exist.");
             } else {
                 Files.lines(logPath, StandardCharsets.UTF_8)
                      .forEach(writer::println);
@@ -83,5 +84,27 @@ public class LogsResource {
         } catch (IOException e) {
             log.error("Exception happened", e);
         }
+    }
+
+    /**
+     * Get the actively used log file from the underlying logging mechanism
+     */
+    private Optional<String> getActiveLogFile() {
+        Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        Iterator<Appender<ILoggingEvent>> itr = rootLogger.iteratorForAppenders();
+
+        // Iterate over the appenders
+        while (itr.hasNext()) {
+            Appender<ILoggingEvent> appender = itr.next();
+
+            // found a file appender
+            if (appender instanceof FileAppender) {
+                FileAppender<ILoggingEvent> fileAppender = (FileAppender<ILoggingEvent>) appender;
+                return Optional.of(fileAppender.getFile());
+            }
+        }
+
+        // no file appender
+        return Optional.absent();
     }
 }
