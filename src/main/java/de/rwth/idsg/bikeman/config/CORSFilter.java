845cfa769700f3f5485baf103c2b708d8be3d181
Michael712 <michael.gitrepo@zwanzig-zehn.de>
2016-01-26 00:30:30 +0100

    Support handling of POST/DELETE CORS and accesses with credentials (add preflight and specify domain name in allow-origin header)

--- src/main/java/de/rwth/idsg/bikeman/config/CORSFilter.java
+++ src/main/java/de/rwth/idsg/bikeman/config/CORSFilter.java
@@ -1,29 +1,52 @@
 package de.rwth.idsg.bikeman.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
+@ConfigurationProperties(prefix="server.cors")
 public class CORSFilter implements Filter {
+
+    private List<String> allowedDomains = new ArrayList<String>();
+
+    public List<String> getAllowedDomains () {
+        return this.allowedDomains;
+    }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if ( !(request instanceof HttpServletRequest) || !(response instanceof HttpServletResponse) ) {
+            throw new ServletException("Non-http requests are not supported!");
+        }
 
-        //TODO: this allows access from _ALL_ origins; allow only specific locations in non-development environments!
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-        chain.doFilter(request, response);
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        String origin = httpRequest.getHeader("origin");
+        if (origin != null) {
+            if (this.getAllowedDomains().contains(origin)) {
+                httpResponse.setHeader("Access-Control-Allow-Origin", origin);
+                httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+                httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+                httpResponse.setHeader("Access-Control-Max-Age", "3600");
+                httpResponse.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+            }
+
+            chain.doFilter(httpRequest, httpResponse);
+        }
     }
 
     public void destroy() {}
 }
+
